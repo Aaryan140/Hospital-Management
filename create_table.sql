0f9927b7b65363f40d95671ff362f0c10980b303
Aryan Tanwar <90985834+Aaryan140@users.noreply.github.com>
2024-02-25 16:10:43 +0530

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,8 +1,55 @@
-CREATE TABLE patient_records (
-    p_no INT AUTO_INCREMENT PRIMARY KEY,
-    p_name VARCHAR(100),
-    p_id INT,
-    age INT,
-    department VARCHAR(100),
-    diagnostic VARCHAR(100)
-);
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hospital
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `patient_records`
+--
+
+DROP TABLE IF EXISTS `patient_records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient_records` (
+  `p_no` int NOT NULL AUTO_INCREMENT,
+  `p_name` varchar(100) DEFAULT NULL,
+  `p_id` int DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `department` varchar(100) DEFAULT NULL,
+  `diagnostic` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`p_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient_records`
+--
+
+LOCK TABLES `patient_records` WRITE;
+/*!40000 ALTER TABLE `patient_records` DISABLE KEYS */;
+INSERT INTO `patient_records` VALUES (1,'Aryan',1001,20,'Cardiology','Hypertension'),(2,'Harsh',1002,23,'Pediatrics','Common Cold'),(3,'Geeta',1003,45,'Orthopedics','Fractured Leg'),(4,'Manish',1004,50,'Dermatology','Eczema'),(5,'Kunal',1005,18,'Neurology','Migraine'),(6,'Ishqi',1006,17,'Cardiology','Hole'),(7,'Annie',1007,21,'Dermatology','Skin'),(8,'Vrinda',1008,20,'Pediatrics','Flu');
+/*!40000 ALTER TABLE `patient_records` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-25 16:08:42
