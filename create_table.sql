62316adfeb453c3f77822dfc9dc01d45053cb5b8
Aryan Tanwar <90985834+Aaryan140@users.noreply.github.com>
2024-02-25 14:44:20 +0530

    Create table sql

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE patient_records (
+    p_no INT AUTO_INCREMENT PRIMARY KEY,
+    p_name VARCHAR(100),
+    p_id INT,
+    age INT,
+    department VARCHAR(100),
+    diagnostic VARCHAR(100)
+);
